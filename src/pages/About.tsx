625a33f78ae15c665c1e977118a11f61d943c4a2
Lucas Dias <lucasinga2000@gmail,com>
2025-06-03 20:45:48 -0300

    1

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,10 @@
-import AboutIntro from '../components/AboutIntro/AboutIntro';
 import SocialMediaSection from '../components/SocialMediaSection/SocialMediaSection';
 import './About.css'; // novo css da página
 
 export default function About() {
   return (
     <div className="about-page">
-      <AboutIntro />
+   
       
       <SocialMediaSection />
       {/* <AboutGallery /> */}
